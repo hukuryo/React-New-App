55a5c2188662c8ad99ebadf9382f21ed2549ae21
hukuryo <ryohei.1998.05.09@gmail.com>
2023-10-08 10:40:38 +0900

    その他修正

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,3 +1,3 @@
 const API_KEY = process.env.API_KEY;
 
-export const API_URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${API_KEY}`;
+export const API_URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=b7b516733fb945e0a34703a8dba3b8c1`;
