a8977eb30664a74ece349cfad708e3c499348e9c
hukuryo <ryohei.1998.05.09@gmail.com>
2023-10-21 19:23:30 +0900

    💄 NotFoundページを編集

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom";
 
 export const NotFound = () => {
   return (
-    <>
-      <h1>お探しのページは見つかりませんでした。</h1>
-      <div>
-        <Link to={`/`}>ホームに戻る</Link>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">
+          お探しのページは見つかりませんでした。
+        </h1>
+        <Link to="/" className="text-blue-500 hover:underline">
+          ホームに戻る
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
