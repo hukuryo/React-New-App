a40e66c2f798d481fcbb2c53f94591005b60b765
hukuryo <ryohei.1998.05.09@gmail.com>
2023-11-13 22:59:21 +0900

    ログイン用のスライスを作成

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import clipReducer from "./clipSlice";
 import favoriteReducer from "./favoriteSlice";
+import userReducer from "./userSlice";
 
 export const store = configureStore({
   reducer: {
     clip: clipReducer,
     favorite: favoriteReducer,
+    user: userReducer,
   },
 });
