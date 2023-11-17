c784f25192d7dea7db02b53082c52ff804a8561e
hukuryo <ryohei.1998.05.09@gmail.com>
2023-11-17 23:39:44 +0900

    ログイン画面をログイン後にいけないようにした

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_API_URL, SUPABASE_API_KEY } from "../lib/client";
 import { Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { login } from "../store/userSlice";
 
 const supabase = createClient(SUPABASE_API_URL, SUPABASE_API_KEY);
@@ -13,8 +13,12 @@ export const Login = () => {
   const [user, setUser] = useState([]);
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const isLogin = useSelector((state) => state.user.user);
 
   useEffect(() => {
+    if (isLogin) {
+      navigate("/");
+    }
     getUsers();
   }, []);
 
